5292edbdf4baa2caf034337af38fb8f8e1ebd14d
Ethan Garrick <ethan.garr2000@gmail.com>
2023-08-17 16:38:49 -0500

    Update repo

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const user_route = require(path.resolve('src/app/user/user.routes.js'));
-const connectDB = require(path.resolve('src/app/config/db/db.config.js'));
 const api_route = require("./src/api/api.routes.js")
 const flash = require('connect-flash')
 require("dotenv").config();
@@ -14,24 +10,15 @@ require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 app.use(morgan('tiny'));
 app.use(flash());
 
-
-app.use(session({
-    secret: process.env.SESSION_SECRET, 
-    saveUninitialized: true, 
-    cookie: {maxAge: 20 * 40 * 60 * 1000}, 
-    resave: false
-}));
-
 app.use(express.json());
 app.use("/tmdb", api_route);
-app.use("/user", user_route);
 
 const SERVER_PORT = process.env.PORT
 
@@ -39,14 +26,8 @@ app.listen(SERVER_PORT, ()=>{
     console.log(`Server started on port: ${SERVER_PORT}`);
 })
 
-connectDB();
-
 app.get('/', (req, res) => {
-    if(req.session){
-        res.render('search', {title:"Home", session:req.session}); 
-    }else{
-        res.redirect('/user/login');
-    }
+    res.render('search', {title:"Home"});
 })
 
 
